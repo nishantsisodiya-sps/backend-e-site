9a8a93df9f0f8fbc612f43a5bc7264cd855330eb
nishantsisodiya-sps <nishantsisodiya.softprodigy@gamil.com>
2023-07-25 19:15:55 +0530

    change in wishlist

--- src/controller/wishlistController.js
+++ src/controller/wishlistController.js
@@ -3,21 +3,74 @@ const Product = require('../models/products')
 
 
 
+// exports.AddProductToWishlist = async (req, res) => {
+//   try {
+   
+//     const userId = req.user ? req.user._id : req.seller._id;
+//     const productId = req.body.productId;
+
+//     const wishlist = await Wishlist.findOne({ userId: userId });
+
+//     if (!wishlist) {
+//       // Wishlist doesn't exist for the user, create a new one
+//       const newWishlist = new Wishlist({
+//         userId: userId,
+//         products: [productId]
+//       });
+//       await newWishlist.save();
+
+//       res.status(200).json({
+//         message: 'Product added to wishlist',
+//         wishlist: newWishlist
+//       });
+//     } else {
+//       const productExists = wishlist.products.includes(productId);
+
+//       if (productExists) {
+//         // Product already exists in the wishlist, remove it
+//         await Wishlist.updateOne({ userId: userId }, { $pull: { products: productId } });
+
+//         res.status(200).json({
+//           message: 'Product removed from wishlist',
+//           wishlist: wishlist
+//         });
+//       } else {
+//         // Product doesn't exist in the wishlist, add it
+//         await Wishlist.updateOne({ userId: userId }, { $push: { products: productId } });
+
+//         wishlist.products.push(productId);
+
+//         res.status(200).json({
+//           message: 'Product added to wishlist',
+//           wishlist: wishlist
+//         });
+//       }
+//     }
+//   } catch (error) {
+//     console.log('Add to Wishlist', error);
+//     res.status(500).json({
+//       error: error
+//     });
+//   }
+// };
+
+
 exports.AddProductToWishlist = async (req, res) => {
   try {
-   
     const userId = req.user ? req.user._id : req.seller._id;
     const productId = req.body.productId;
 
     const wishlist = await Wishlist.findOne({ userId: userId });
 
     if (!wishlist) {
-      // Wishlist doesn't exist for the user, create a new one
       const newWishlist = new Wishlist({
         userId: userId,
         products: [productId]
       });
       await newWishlist.save();
+
+      // Set the isWishlist flag to true for the product
+      await Product.findByIdAndUpdate(productId, { isWishlist: true });
 
       res.status(200).json({
         message: 'Product added to wishlist',
@@ -27,18 +80,22 @@ exports.AddProductToWishlist = async (req, res) => {
       const productExists = wishlist.products.includes(productId);
 
       if (productExists) {
-        // Product already exists in the wishlist, remove it
         await Wishlist.updateOne({ userId: userId }, { $pull: { products: productId } });
+
+        // Set the isWishlist flag to false for the product
+        await Product.findByIdAndUpdate(productId, { isWishlist: false });
 
         res.status(200).json({
           message: 'Product removed from wishlist',
           wishlist: wishlist
         });
       } else {
-        // Product doesn't exist in the wishlist, add it
         await Wishlist.updateOne({ userId: userId }, { $push: { products: productId } });
 
         wishlist.products.push(productId);
+
+        // Set the isWishlist flag to true for the product
+        await Product.findByIdAndUpdate(productId, { isWishlist: true });
 
         res.status(200).json({
           message: 'Product added to wishlist',
@@ -53,11 +110,6 @@ exports.AddProductToWishlist = async (req, res) => {
     });
   }
 };
-
-
-
-
-
 
 exports.GetWishlist = async (req, res) => {
   try {
@@ -80,8 +132,6 @@ exports.GetWishlist = async (req, res) => {
     });
   }
 };
-
-
 
 
 
